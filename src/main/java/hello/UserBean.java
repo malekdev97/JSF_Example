8b659b25b76d4fef5327690ca7a293f6978f824f
Abdulmalik <abdulmalik-abdullah@hotmail.com>
2025-05-30 01:35:10 +0300

    list actions selector

--- src/main/java/hello/UserBean.java
+++ src/main/java/hello/UserBean.java
@@ -1,87 +1,21 @@
 package hello;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.List;
-import hello.model.*;
+import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name = "userBean")
-@RequestScoped
-public class UserBean {
-	
-	private List<Student> usersList = new ArrayList<Student>(Arrays.asList(new Student(100, "Malek"), new Student(200, "Fahad")));
-	
-	public List<Student> getUsersList() {
-		return usersList;
-	}
+@ManagedBean(name = "userData", eager = true)
+@SessionScoped
+public class UserBean implements Serializable {
+   private static final long serialVersionUID = 1L;
+   public String[] data = {"1","2","3"};
 
-	public void setUsersList(List<Student> usersList) {
-		this.usersList = usersList;
-	}
+   public String[] getData() {
+      return data;
+   }
 
-	private String email; // Text Input
-    private String country; // DropDown List
-    private List<String> interests = new ArrayList<>(); // CheckBox
-    private String contactMethod; // Radio
-    private List<String> selectedSkills; // Multiple Selection DropDown List
-
-	public void init() {
-        contactMethod = "email"; // Default to email
-    }
-    
-    //==== Views ====//
-	public String formView() {
-    	return "user-form";
-    }
-    
-    
-    //==== Actions ====//
-    public String submitForm() {
-        return "user-response";
-    }
-
-    //==== Getters and Setters ====//
-    public List<String> getSelectedSkills() {
-		return selectedSkills;
-	}
-
-	public void setSelectedSkills(List<String> selectedSkills) {
-		this.selectedSkills = selectedSkills;
-	}
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public List<String> getInterests() {
-        return interests;
-    }
-
-    public void setInterests(List<String> interests) {
-        this.interests = interests;
-    }
-
-    public String getContactMethod() {
-        return contactMethod;
-    }
-
-    public void setContactMethod(String contactMethod) {
-        this.contactMethod = contactMethod;
-    }
+   public void setData(String[] data) {
+      this.data = data;
+   }
 }
